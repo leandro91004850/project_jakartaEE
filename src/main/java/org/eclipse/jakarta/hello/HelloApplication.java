2beb0c7b9f92295c38d39d341a1cab72bdf7c0f9
leandroleekrap <leandroleekrap@gmail.com>
2024-08-17 15:39:58 -0300

    Firt commit

---
+++ src/main/java/org/eclipse/jakarta/hello/HelloApplication.java
@@ -0,0 +1,9 @@
+package org.eclipse.jakarta.hello;
+
+import javax.ws.rs.core.Application;
+import javax.ws.rs.ApplicationPath;
+
+@ApplicationPath("rest")
+public class HelloApplication extends Application {
+    
+}
